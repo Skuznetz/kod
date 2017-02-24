6a0a9f445b247a894e667bb6631eba78f114026d
kuznetz <s.kuznetzov@mail.ru>
2017-02-24 12:53:47 +0300

    yield

--- init.rb
+++ init.rb
@@ -1,13 +1,5 @@
 require_relative "cart"
 require_relative "item"
 
-cart = Cart.new
-cart.add_item(Item.new)
-cart.add_item(Item.new)
-
-p cart.items
-
-cart.remove_item
-p cart.items
-
-cart.validate
+item = Item.new({:price =>10, :weight =>100, :name =>"bar"})
+item.info {|attr| puts attr}
