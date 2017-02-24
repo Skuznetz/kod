cd168eaa76509d785ea5641acd45508f9e92a733
kuznetz <s.kuznetzov@mail.ru>
2017-02-24 10:52:22 +0300

    add card

---
+++ Cart.rb
@@ -0,0 +1,14 @@
+﻿class Cart
+  attr_reader :items
+   def initialize
+     items = Array.new 
+   end
+
+   def add_item(item)
+   	@item.push item
+   end
+
+   def remove_item
+   	 @item.pop
+   end
+end
