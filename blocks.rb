5f609ecd101932fc89d459e3f93ff30ced7fa011
kuznetz <s.kuznetzov@mail.ru>
2017-02-24 12:53:47 +0300

    file.txt

--- blocks.rb
+++ blocks.rb
@@ -1,2 +1,8 @@
 names =["Иван","Пётр","Егор"]
 names.each {|name| puts name}
+
+
+ # File.open "/tmp/blocks.txt", "w" { |f| f.puts "hello world"}
+a = File.open "blocks.txt","w"
+a.write "port"
+a.close
