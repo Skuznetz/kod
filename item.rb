93887e6a813c9e9228d49d8b3418e3742ec33b76
kuznetz <s.kuznetzov@mail.ru>
2017-02-26 10:08:32 +0300

    method tax

--- item.rb
+++ item.rb
@@ -1,10 +1,11 @@
 class Item
-
+  
+  @@discount = 0.1
 	def self.discount
 		if Time.now.month == 4
-			0.3
+			return @@discount + 0.2
 		else
-			0.1
+			return @@discount
 		end
 	end
   def initialize(options={})
@@ -23,6 +24,19 @@ class Item
   end
 
   def price
-  	@price - @price*self.class.discount
+  	(@price - @price*self.class.discount) + tax
   end
+
+  def tax
+  	type_tax = if self.class == VirtualItem
+  		1
+  	else
+  		2
+  	end
+  	cost_tax = if @price > 5
+  		@price*0.2
+  	else
+  		@prise*0.1
+  	end
+  	cost_tax + type_tax
 end
