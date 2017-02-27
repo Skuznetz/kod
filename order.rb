7867922425e8621ef3573e0b4db27ab06f26080f
kuznetz <s.kuznetzov@mail.ru>
2017-02-27 19:51:22 +0300

    class order

---
+++ order.rb
@@ -0,0 +1,17 @@
+class Order
+	def add_item(item)
+   	@items.push item
+   end
+
+   def remove_item
+   	 @items.pop
+   end
+
+   def validate
+   	@items.each {|i| puts "Не указана цена" if i.price.nil?}
+   end
+
+   def delete_invalid_items
+   	@items.delete_if {|i| i.price.nil?}
+   end
+end
