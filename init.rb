c4ac1761ffb89cb44995a68264c22ded67a9a4c3
kuznetz <s.kuznetzov@mail.ru>
2017-02-28 19:49:36 +0300

    add array

--- init.rb
+++ init.rb
@@ -5,13 +5,8 @@ require_relative "item"
 require_relative "virtual_item"
 require_relative "real_item"
 
-item1 = VirtualItem.new({:price =>101, :weight =>100, :name =>"bar"})
+@items = []
+@items << VirtualItem.new({:price =>101, :weight =>100, :name =>"bar"})
 
-item2 = RealItem.new({:weight =>100,:price =>101,:name =>"bar"})
-item3 = RealItem.new({:weight =>100,:price =>10, :name =>"pup"})
- cart = Cart.new
- cart.add_item item1
- cart.add_item item2
- cart.add_item item3
-
- puts cart.items.size
+@items << RealItem.new({:weight =>100,:price =>101,:name =>"bar"})
+@items << RealItem.new({:weight =>100,:price =>10, :name =>"pup"})
