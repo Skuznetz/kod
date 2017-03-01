503dede876c119e98cc853970c1c91da10fa04d0
kuznetz <s.kuznetzov@mail.ru>
2017-03-01 17:49:01 +0300

    file string.rb

--- сart.rb
+++ сart.rb
@@ -18,9 +18,6 @@
    def read_from_file
    	return unless File.exists?("#{@owner}_cart.txt")
    	item_fields = File.readlines("#{@owner}_cart.txt")
-   	item_fields.map! { |i| i.chomp}
-   	item_fields.map! {|i| i.split(":")}
-   	item_fields.each { |i| @items << RealItem.new(name: i[0],price: i[1].to_i,weight: i[2].to_i)}
    	
    	@items.uniq!
    end
