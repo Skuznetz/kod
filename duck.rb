bd87ff476982e396e973a141adbd4ebc0377607f
kuznetz <s.kuznetzov@mail.ru>
2017-03-07 13:18:26 +0300

    @@greeting

--- duck.rb
+++ duck.rb
@@ -1,15 +1,23 @@
 class Duck
+	@@greeting = "hello"
   def initialize(greeting)
   	@greeting = greeting
   end
   def quack
-  	puts @greeting
+  	puts "#{@@greeting} #{@greeting}"
   end
-  def self.quack
-    puts "Class quack"
-end
+
+  class << self
+
+  	attr_accessor :greeting
+
+    def quack
+    puts "#{@@greeting} #{@greeting}"
+    end
+  end
 end
 
 duck1 = Duck.new("quack quack")
 duck1.quack
+Duck.greeting = "class quack"
 Duck.quack
