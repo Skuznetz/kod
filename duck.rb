ad52d9163cb71a79ea4b79296281a9bc58b5a1e3
kuznetz <s.kuznetzov@mail.ru>
2017-03-07 12:42:02 +0300

    method initialize

--- duck.rb
+++ duck.rb
@@ -1,12 +1,15 @@
 class Duck
+  def initialize(greeting)
+  	@greeting = greeting
+  end
   def quack
-  	puts "quack quack"
+  	puts @greeting
   end
   def self.quack
     puts "Class quack"
 end
 end
 
-duck1 = Duck.new
+duck1 = Duck.new("quack quack")
 duck1.quack
 Duck.quack
