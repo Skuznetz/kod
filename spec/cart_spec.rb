486dc51cb6d6ddaf83b6b19000496f72bcd595cc
kuznetz <s.kuznetzov@mail.ru>
2017-03-08 20:10:23 +0300

    describe Cart

---
+++ spec/cart_spec.rb
@@ -0,0 +1,17 @@
+require "rspec"
+require_relative "../app/item"
+require_relative "../app/virtual_item"
+require_relative "../app/item_container"
+require_relative "../app/cart"
+
+describe Cart do 
+
+  it "adds items into the cart" do 
+  	cart = Cart.new
+  	item1 = Item.new("kettle",price: 200)
+  	item2 = Item.new("bar",price: 200)
+  	cart.add_items(item1,item2)
+  	cart.items.should include(item1,item2)
+  end
+
+end
