663f1c2ba790a300fdba1f54611421b77cd4ebda
kuznetz <s.kuznetzov@mail.ru>
2017-03-10 11:13:21 +0300

    before (:each)

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -3,15 +3,15 @@ require_relative "../app/item"
 require_relative "../app/virtual_item"
 describe Item do 
 
-	
+	before(:each) do 
+	  @item = Item.new("kettle",price: 200)
+	end
 
 	it "calculates price according to a special formula" do
-	 item = Item.new("kettle",price: 200)
-	@item.price.should == 222.0
+	 @item.price.should == 222.0
     end
 
     it "return info on object" do 
-      item = Item.new("kettle",price: 200)
-      @item.to_s.should == "kettle:222.0"
+    @item.to_s.should == "kettle:222.0"
   end
 end
