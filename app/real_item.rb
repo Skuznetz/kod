d78ac772fef547534d6c5d5fb2da9cc6fa54ce48
kuznetz <s.kuznetzov@mail.ru>
2017-03-10 11:11:35 +0300

    method to_s

--- app/real_item.rb
+++ app/real_item.rb
@@ -10,4 +10,9 @@ class RealItem < Item
   	yield(weight)
   	super
   end
+
+  def to_s
+  	super +":#{self.weight}"
+  end
+
 end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -3,8 +3,15 @@ require_relative "../app/item"
 require_relative "../app/virtual_item"
 describe Item do 
 
+	
+
 	it "calculates price according to a special formula" do
-	item = Item.new( "kettle", price: 200 ) 
-	item.price.should == 222.0
+	 item = Item.new("kettle",price: 200)
+	@item.price.should == 222.0
     end
+
+    it "return info on object" do 
+      item = Item.new("kettle",price: 200)
+      @item.to_s.should == "kettle:222.0"
+  end
 end
